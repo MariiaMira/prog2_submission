0fd2d77d03339b531340c8ac38016c92e558a0a3
MariiaMira <roze2117@gmail.com>
2024-04-23 10:10:51 +0200

    added files

--- src/Edge.java
+++ src/Edge.java
@@ -1,2 +1,11 @@
 public class Edge <T>{
+    private final T destination;
+    private final String name;
+    private int weight;
+
+    public Edge(T destination, String name, int weight) {
+        this.destination = destination;
+        this.name = name;
+        this.weight = weight;
+    }
 }
