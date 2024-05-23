86f5d217c46f0be072092a6d3ce56c03799bf7df
Sebastian Christiansson <Sebastianch8@gmail.com>
2024-05-23 11:53:12 +0200

    location is back

--- src/Location.java
+++ src/Location.java
@@ -1,9 +1,6 @@
-import javafx.event.EventHandler;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Label;
-import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.text.TextAlignment;
@@ -12,29 +9,23 @@ public class Location extends Canvas {
     private String name;
     private double x;
     private double y;
-    private Circle circle;
-    private Label label;
-    private GraphicsContext gContext = getGraphicsContext2D();
+    private GraphicsContext graphicsContext = getGraphicsContext2D();
 
     public Location(String name, double x, double y){
         super(60, 60);
         this.name = name;
         this.x = x;
         this.y = y;
-        this.circle = new Circle(8,Color.BLUE);
-        this.label = new Label(name);
-
-
         //gContext.setFill(Color.BLACK);
-        gContext.fillText(name, 0, 30);
-        gContext.setTextAlign(TextAlignment.JUSTIFY);
+        graphicsContext.fillText(name, 0, 30);
+        graphicsContext.setTextAlign(TextAlignment.JUSTIFY);
         paintCovered(Color.BLUE);
 
     }
 
     public void paintCovered(Color color){
-        gContext.setFill(color);
-        gContext.fillOval(0,0,16,16);
+        graphicsContext.setFill(color);
+        graphicsContext.fillOval(0,0,16,16);
     }
 
     public String getName(){
