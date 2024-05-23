aa87f9501e532e2c74c9424387b06024ea377cd6
MariiaMira <roze2117@gmail.com>
2024-05-23 11:40:08 +0200

    updated

--- src/Location.java
+++ src/Location.java
@@ -1,12 +1,9 @@
-import javafx.event.EventHandler;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Label;
-import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
-import javafx.scene.text.TextAlignment;
+
 
 public class Location extends Canvas {
     private String name;
@@ -14,26 +11,24 @@ public class Location extends Canvas {
     private double y;
     private Circle circle;
     private Label label;
-    private GraphicsContext gContext = getGraphicsContext2D();
+    private GraphicsContext graphicsContext = getGraphicsContext2D();
 
     public Location(String name, double x, double y){
         super(75, 75);
         this.name = name;
         this.x = x;
         this.y = y;
-        this.circle = new Circle(8,Color.BLUE);
+        this.circle = new Circle(8, Color.BLUE);
         this.label = new Label(name);
 
-
-        //gContext.setFill(Color.BLACK);
-        gContext.fillText(name, 0, 30);
+        // Draw the name and cover circle
+        graphicsContext.fillText(name, 0, 30);
         paintCovered();
-
     }
 
     public void paintCovered(){
-        gContext.setFill(Color.BLUE);
-        gContext.fillOval(0,0,16,16);
+        graphicsContext.setFill(Color.BLUE);
+        graphicsContext.fillOval(0, 0, 16, 16);
     }
 
     public String getName(){
@@ -48,18 +43,8 @@ public class Location extends Canvas {
         return y;
     }
 
+    @Override
     public String toString(){
         return name + "[" + x + ", " + y + "]";
     }
-
-    // förändra färg vid klick
-    /*class clickHandler implements EventHandler<MouseEvent>{
-        @Override
-        public void handle(MouseEvent mouseEvent) {
-            circle.setFill(Color.RED);
-            requestFocus();
-        }
-    }
-    */
-
 }
