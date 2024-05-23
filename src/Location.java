c1b0e3470d05a663bf6fe4c88bf440f92c625260
Sebastian Christiansson <Sebastianch8@gmail.com>
2024-05-23 11:29:02 +0200

    location back

--- src/Location.java
+++ src/Location.java
@@ -1,24 +1,37 @@
 import javafx.event.EventHandler;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 
-public class Location extends VBox {
+public class Location extends Canvas {
     private String name;
     private double x;
     private double y;
     private Circle circle;
     private Label label;
+    private GraphicsContext gContext = getGraphicsContext2D();
 
     public Location(String name, double x, double y){
+        super(75, 75);
         this.name = name;
         this.x = x;
         this.y = y;
         this.circle = new Circle(8,Color.BLUE);
         this.label = new Label(name);
-        getChildren().addAll(circle, label);
+        //getChildren().addAll(circle, label);
+        paintCovered();
+        gContext.fillOval(0,0,16,16);
+        gContext.setFill(Color.BLACK);
+        gContext.fillText(name, 0, 30);
+
+    }
+
+    public void paintCovered(){
+        gContext.setFill(Color.BLUE);
     }
 
     public String getName(){
